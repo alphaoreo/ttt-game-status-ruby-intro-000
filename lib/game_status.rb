2a59a6051d5cad69bbcfb72dbe980f02468f8243
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-05-06 01:17:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
+  
   [0,1,2]
   [3,4,5]
   [6,7,8]
@@ -13,7 +14,8 @@ WIN_COMBINATIONS = [
   [0,3,6]
   [1,4,7]
   [2,5,8]
-]]
+  
+]
 
 def won?(board)
   for each win_combination in WIN_COMBINATIONS
