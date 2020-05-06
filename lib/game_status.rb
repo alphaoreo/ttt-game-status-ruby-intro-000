1b678f5b95e3d4dfe89e925575c9c73ac302f77d
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-05-06 01:35:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     win_index_1= win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    if board[win_index_3] == board[win_index_2] && board[win_index_2] == board[win_index_1] && (board[win_index_1] != "" || board[win_index_1] != " ")
+    if board[win_index_3] == board[win_index_2] && board[win_index_2] == board[win_index_1] && board[win_index_1] == "X"
       return win_combination
     end
   end
